9bc0c8eb054e3ffd7351d61c94c0d04cfb55036e
Carlos <carlosaguinaga01@gmail.com>
2022-03-07 14:54:11 -0500

    route category added

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const { productsRouter } = require('./routers/products.router');
 const { db } = require('./db/connection');
+const { categoriesRouter } = require('./routers/categories.router');
+
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 const conectarDB = async () => {
   try {
-      await db.authenticate();
-      console.log('Base de datos online')
+    await db.authenticate();
+    console.log('Base de datos online');
   } catch (error) {
-      throw new Error(error)
+    throw new Error(error);
   }
 };
 
@@ -20,5 +25,6 @@ app.get('/api/v1', (req, res) => {
 });
 
 app.use('/api/v1/products', productsRouter);
+app.use('/api/v1/categories', categoriesRouter);
 
 module.exports = { app };
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,26 +1,41 @@
 const { db } = require('../db/connection');
+const { Op } = require('sequelize');
 const { Product } = require('../models/product');
 const { Category } = require('../models/category');
 
 const getAllProducts = async (req, res) => {
   // const products = await db.query('SELECT * FROM product');
+  console.log(req.query);
+
+  const whereOptions = {};
+
+  if (req.query.category) {
+    whereOptions.category = req.query.category;
+  }
+  
+  if (req.query.search) {
+    console.log('in search', req.query.search)
+    whereOptions.name = {
+      [Op.like]: `%${req.query.search}%`,
+    };
+  }
+  
 
   try {
-    const productos = await Product.findAll({
-      where: {
-        category: 5,
-      },
+    const productos = await Product.findAndCountAll({
+      where: whereOptions,
     });
-    res.json({ msg: productos });
+    res.json( productos );
   } catch (error) {
     console.log(error);
   }
 };
 
 const getOneProduct = async (req, res) => {
+  const params = req.query;
   const id = req.params.id;
-  const category = await Category.findAll();
-  res.json({ msg: category });
+  const producto = await Product.findByPk(id);
+  res.json({ msg: producto });
 };
 
 module.exports = {
